36507467fa228044f9ea1ab0fa13d85d0291fbfb
sekai882 <josuemullo10@hotmail.com>
2026-04-09 01:11:34 -0500

    Initial commit: Spring Boot configurado con SQL Server y dependencias

---
+++ src/main/java/com/udla/futbol/FutbolManagerCrudApplication.java
@@ -0,0 +1,13 @@
+package com.udla.futbol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutbolManagerCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FutbolManagerCrudApplication.class, args);
+    }
+
+}
